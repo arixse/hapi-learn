05c6e670ed866b45aff5a057da9967c13abff994
fusheng <fusheng@netposa.com>
2019-05-31 09:49:53 +0800

    sequelize

--- config/config.js
+++ config/config.js
@@ -1,25 +1,24 @@
-
-if(process.env.NODE_ENV=='production') {
-  require('env2')('./.env.prod')
+if (process.env.NODE_ENV == "production") {
+	require("env2")("./.env.prod");
 } else {
-  require('env2')('./.env')
+	require("env2")("./.env");
 }
-const {env} = process;
+const { env } = process;
 module.exports = {
-development: {
-  username: env.DB_USERNAME,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME,
-  host: env.DB_HOST,
-  port:env.DB_PORT,
-  dialect: "mysql"
-},
-production: {
-  username: env.DB_USERNAME,
-  password: env.DB_PASSWORD,
-  database: env.DB_DB_NAME,
-  host: env.DB_HOST,
-  port:env.DB_PORT,
-  dialect: "mysql"
-}
+	development: {
+		username: env.DB_USERNAME,
+		password: env.DB_PASSWORD,
+		database: env.DB_NAME,
+		host: env.DB_HOST,
+		port: env.DB_PORT,
+		dialect: "mysql"
+	},
+	production: {
+		username: env.DB_USERNAME,
+		password: env.DB_PASSWORD,
+		database: env.DB_DB_NAME,
+		host: env.DB_HOST,
+		port: env.DB_PORT,
+		dialect: "mysql"
+	}
 };
