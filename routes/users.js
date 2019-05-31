d26b0225251db0d09178c9b8a4e7ea0f5acd927e
fusheng <fusheng@netposa.com>
2019-06-01 00:41:58 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const JWT = require('jsonwebtoken')
+const joi = require('joi')
 const GROUP_NAME = `users`
 
 module.exports = [
@@ -6,7 +7,7 @@ module.exports = [
         method:'POST',
         path:`/${GROUP_NAME}`,
         handler:async (request,reply) => {
-            function generaateJWT(jwtInfo) {
+            function generateJWT(jwtInfo) {
                 const payload = {
                     userId:jwtInfo.userId,
                     exp:Math.floor(new Date().getTime()/1000 + 7*24*60*60)
@@ -14,7 +15,7 @@ module.exports = [
                 return JWT.sign(payload,process.env.JWT_SECRET)
             }
             reply(
-                generaateJWT({
+                generateJWT({
                     userId:1
                 })
             )
@@ -23,6 +24,24 @@ module.exports = [
             tags:['api',GROUP_NAME],
             description:'测试jwt签发',
             auth:false//约定测接口不参与jwt验证
+        }
+    },
+    {
+        method:'POST',
+        path:`/${GROUP_NAME}/wxLogin`,
+        handler:async (request,reply)=>{
+            reply()
+        },
+        config: {
+            auth:false,
+            tags:['api',GROUP_NAME],
+            validate:{
+                payload:{
+                    code:joi.string().required().description('微信用户登陆的临时的code'),
+                    encryptedData:joi.string().required().description('微信用户信息cryptedData'),
+                    iv:joi.string().required().description('微信用户信息iv')
+                }
+            }
         }
     }
 ]
