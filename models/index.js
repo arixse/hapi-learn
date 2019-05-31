9d2b06afd0f70e6110f117289821b4c315ac4c3b
fusheng <fusheng@netposa.com>
2019-05-31 11:06:16 +0800

    model

--- models/index.js
+++ models/index.js
@@ -5,9 +5,14 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const configs = require(__dirname + '/../config/config.js')[env];
 const db = {};
-
+const config = {
+  ...configs,
+  define:{
+    underscored:true
+  }
+}
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
