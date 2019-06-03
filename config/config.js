9951481a2a998ad41b69183011248c3ace7fb26c
fusheng <fusheng@netposa.com>
2019-06-03 15:14:06 +0800

    fix

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
 	production: {
 		username: env.DB_USERNAME,
 		password: env.DB_PASSWORD,
-		database: env.DB_DB_NAME,
+		database: env.DB_NAME,
 		host: env.DB_HOST,
 		port: env.DB_PORT,
 		dialect: "mysql"
